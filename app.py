f720dd714b2710d403bb9745c57ee874aecb2aab
Felix <felixm@felixs-mbp.home>
2023-01-24 10:20:11 +0000

    text edit

--- app.py
+++ app.py
@@ -5,13 +5,12 @@ from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 df = pd.read_csv('sentiment.csv')
 sent_df = df[['year','anger', 'anticipation','disgust', 'fear', 'joy','negative', 'positive', 'sadness', 'surprise', 'trust' ]].set_index('year')
 st.title('Christmas Speech Sentiment Analysis')
-st.text('Royal Christmas Speeches 1997-2022')
-st.text('Pick an emotion/emotions and see the corrensponding scores')
+st.write('The following analysis of the Royal Christmas Speeches 1997-2022 was completed using the NRC Lexicon to generate a score for a series of emotions. Select some emotions from the drop down menu to see the corresponding scores by year on the graph below.')
 columns = st.multiselect(
     'Pick some emotions',
     ['anger', 'anticipation','disgust', 'fear', 'joy','negative', 'positive', 'sadness', 'surprise', 'trust' ],
     ['trust', 'fear'])
-st.line_chart(sent_df[columns])
+st.line_chart(sent_df[columns], use_container_width=True)
 # st.text('Click on column title to change order')
 # sent_df
 st.subheader('Pick a year to see a wordcloud')
